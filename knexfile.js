2c46ab034b06e00f8a05aa541bf01d3e8660a6fa
gchu <gchu647@gmail.com>
2018-09-27 10:59:24 -1000

    configured knexfile to production

--- knexfile.js
+++ knexfile.js
@@ -45,17 +45,22 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: null
+      database: 'cms_db',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
+      directory: path.join(__dirname, 'server', 'db', 'migrations'),
       tableName: 'knex_migrations'
-    }
+    },
+    seeds: {
+      directory: path.join(__dirname, 'server', 'db', 'seeds')
+    },
+    debug: false,
   }
 
 };
